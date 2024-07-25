c4dbe979dd239618157894581ed60182134c0dfa
Hwang gend <ghkdtjsdks@naver.com>
2024-07-25 16:29:18 +0900

    [UPDATE] 타이머/스톱워치 모드변경 버튼 구현

--- Week1/Sunan/timer/src/App.js
+++ Week1/Sunan/timer/src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function Timer() {
+  return <h1>타이머입니다.!</h1>;
+}
+
 function App() {
+  const [mode, setMode] = useState('TIMER');
+  let content = null;
+
+  if (mode === 'TIMER') {
+    content = <Timer />;
+  } else {
+    content = <Timer />;
+  }
+//className ="" 적는것은 요소에 CSS 클래스를 적용하는데에 사용된다고
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="item">
+        <a href='/timer' onClick={(event)=>{
+          event.preventDefault();
+          setMode('TIMER');
+        }} className={`mode ${mode === 'TIMER' ? 'active' : ''}`}><p>타이머</p></a>
+        <hr width='100%' color='black'></hr>
+        <a href='/stopwatch' onClick={(event)=>{
+          event.preventDefault();
+          setMode('STOPWATCH');
+        }} className={`mode ${mode === 'STOPWATCH' ? 'active' : ''}`}><p>스톱워치</p></a>
+      </div>
+      <div className="item">{content}</div>
+      <div className="item">{content}</div>
     </div>
   );
 }
