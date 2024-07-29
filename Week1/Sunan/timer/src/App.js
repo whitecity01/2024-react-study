bd0aef87264ad7c945a1cc047c2a387d2065feb1
Hwang gend <ghkdtjsdks@naver.com>
2024-07-29 15:46:27 +0900

    [UPDATE] 스톱워치 구현 완료

--- Week1/Sunan/timer/src/App.js
+++ Week1/Sunan/timer/src/App.js
@@ -6,6 +6,7 @@ import {FaStopCircle, FaPlusCircle, FaPauseCircle, FaPlayCircle} from 'react-ico
 function Stopwatch() {
   const [time, setTime] = useState(0);
   const [start, setStart] = useState(false);
+  const [records, setRecords] = useState([]);
 
   useEffect(() => {
     let interval;
@@ -24,21 +25,26 @@ function Stopwatch() {
   const handleStart = () => {
     setStart(true);
   };
+  
   const handleRecord = () => {
-    setTime(0);
+    const newRecord = `0${Math.floor((time / 60000) % 60)}`.slice(-2) + " : " +
+                      `0${Math.floor((time / 1000) % 60)}`.slice(-2) + " : " +
+                      `0${Math.floor((time / 10) % 100)}`.slice(-2);
+    setRecords([...records, newRecord]); // 현재 시간을 기록에 추가
   };
   const handleStop = () => {
     setStart(false);
   };
-
+  
   const handleReset = () => {
     setTime(0);
     setStart(false);
+    setRecords([]);
   };
-
   return (
-    <div>
-      <div><h1>스톱워치</h1></div>
+    <div className='container'>
+      <div className='item'>
+        <div><h1>스톱워치</h1></div>
       <time>
         {`0${Math.floor((time / 60000) % 60)}`.slice(-2)} :
         {`0${Math.floor((time / 1000) % 60)}`.slice(-2)} :
@@ -51,11 +57,26 @@ function Stopwatch() {
         <button onClick={handleReset}><FaStopCircle className='icon'/></button>
       </div>
     </div>
+    <div><hr className="line"></hr></div>
+    <div className='records'>
+        <h2>기록</h2>
+        <ul>
+          {records.map((record, index) => (
+            <li key={index}>{record}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+    
   );
 }
 
 function Timer() {
   return <h1>타이머입니다.!</h1>;
+}
+
+function Recorder(){
+
 }
 
 function App() {
@@ -83,8 +104,6 @@ function App() {
       </div>
       <div><hr className="line"></hr></div>
       <div className="item">{content}</div>
-      <div><hr className="line"></hr></div>
-      <div className="item">시간기록 부분</div>
     </div>
   );
 }
