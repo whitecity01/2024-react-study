3d22f39577dc047eb4a637bdb6478023e22c13a9
Jiwon Park <parkjiwon0821@naver.com>
2024-08-02 19:45:58 +0900

    files reupload

---
+++ Week1/parkjiwon/src/components/RecordItem.js
@@ -0,0 +1,14 @@
+import './RecordItem.scss';
+
+// 시간 기록 컴포넌트 record를 받아와 표시
+const RecordItem = ({record}) =>{
+    const {text} = record;
+
+    return(
+        <div className='RecordItem'>
+            {text}
+        </div>
+    );
+}
+
+export default RecordItem;
