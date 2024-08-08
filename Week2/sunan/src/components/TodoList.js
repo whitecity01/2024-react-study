35216da239d84db8e9a17f15466eb9ec312ea201
Hwang gend <ghkdtjsdks@naver.com>
2024-08-08 17:43:40 +0900

    [UPDATE] checkbox 스타일 수정

--- Week2/sunan/src/components/TodoList.js
+++ Week2/sunan/src/components/TodoList.js
@@ -5,7 +5,7 @@ function TodoList({ id, task, done, onDelete, onFinish }) {
   return (
     <div className='todobox'>
       <div className="checkboxContainer">
-        <input className='checkbox' type='checkbox' id={id} name='todo' value={id} onClick={()=> onFinish(id)}/>
+        <input type='checkbox' id={id} className='checkbox' onClick={()=> onFinish(id)}/>
         <label htmlFor={id} className={done? 'strikethrough':''}> {task} </label> 
       </div>
       <button className="deleteButton" onClick={() => onDelete(id)}>
