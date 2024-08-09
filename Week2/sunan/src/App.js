4bcf8b91630f020d6d658e62acf26f20649f688d
Hwang gend <ghkdtjsdks@naver.com>
2024-08-10 01:11:32 +0900

    [FIX] jsx 태그 수정

--- Week2/sunan/src/App.js
+++ Week2/sunan/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-//import {useState} from 'react';
 import InputJob from './components/InputJob';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div>
-      <div className='container'>
+    <div className='container'>
         <div className='title'>일정관리</div>
         <InputJob/>
-      </div>
     </div>
   );
 }
