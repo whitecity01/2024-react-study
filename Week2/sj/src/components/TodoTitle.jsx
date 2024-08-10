7322fcade96a17c6b2d3c038f8e82732ad401d0a
whitecity01 <kh081104@gmail.com>
2024-08-11 00:09:35 +0900

    feat: 누락된 todoTitle 컴포넌트 추가

---
+++ Week2/sj/src/components/TodoTitle.jsx
@@ -0,0 +1,10 @@
+import "../styles/TodoTitle.scss";
+const TodoTitle = () => {
+  return (
+    <div className="todo-title-container">
+      <span className="todo-title">일정 관리</span>
+    </div>
+  );
+};
+
+export default TodoTitle;
